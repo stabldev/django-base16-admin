12b6b18b832dfa37d6c1f4111600bb7440fc39f6
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-03 10:40:36 +0530

    feat: add cupcake theme

--- django_demo/django_demo/settings.py
+++ django_demo/django_demo/settings.py
@@ -68,3 +68,6 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Base16 theme
+BASE16_THEME = "cupcake"
